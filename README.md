20de199e25012cbbf18858348dba366ab0b280fb
Cassaundra <cass@cassaundra.io>
2019-02-27 11:22:59 -0800

    Add repo url in footer, update README

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # cassaundra.io
 
-This repository contains the source code for my site, which can be viewed live [here](https://cassaundra.io). There's nothing too interesting here yet.
-
-Run `npm start` for a development view of the site or `npm run build` for a production-optimized build.
+This repository contains the source code for my site, which can be viewed live [here](https://cassaundra.io).
--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -29,6 +29,9 @@ class Page extends Component {
 					<p>BTC: <code>13wXDHFfLw4UVgfMY6Md9XQRp6mAnbLTGf</code></p>
 			 		<p>XMR: <code>8AXRLjdDLuaW6RK4fU9NvRgPQh6Ri2qT3ZkHXPrptrFzfmLZw75kra1VLEWwzEt4jYBUEHA5bG7k3GU4a1XbMJNPN9ExY3q</code></p>
 				</div>
+				<div className="source-footer">
+					<p><a href="https://github.com/cassaundra/cassaundra-io" target="_blank">Hosted with GitHub</a></p>
+				</div>
             </div>
         );
     }
