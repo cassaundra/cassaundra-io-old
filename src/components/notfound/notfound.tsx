0b3211c9793d271fd980173a3a6f1e2d3756bcd7
Cassaundra <cass@cassaundra.io>
2019-02-27 09:20:37 -0800

    Fix indentation

--- src/components/notfound/notfound.tsx
+++ src/components/notfound/notfound.tsx
@@ -6,9 +6,9 @@ class NotFound extends Component {
     render() {
         return (
             <Page>
-							<h1 style={{textAlign: "center"}} className="display-3">404</h1>
-							<p style={{textAlign: "center"}}>:-(</p>
-						</Page>
+				<h1 style={{textAlign: "center"}} className="display-3">404</h1>
+				<p style={{textAlign: "center"}}>:-(</p>
+			</Page>
         );
     }
 }
