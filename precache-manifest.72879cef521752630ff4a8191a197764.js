c0e5bc33c13865d90657dd7fe171c197497fd0b6
Cassaundra <cass@cassaundra.io>
2019-02-27 09:17:04 -0800

    Updates

---
+++ precache-manifest.72879cef521752630ff4a8191a197764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59945baece5d664df450",
+    "url": "/cassaundra-io/static/js/runtime~main.59945bae.js"
+  },
+  {
+    "revision": "8b29a360aa010bc7d089",
+    "url": "/cassaundra-io/static/js/main.8b29a360.chunk.js"
+  },
+  {
+    "revision": "8b8ad942310224a16029",
+    "url": "/cassaundra-io/static/js/2.8b8ad942.chunk.js"
+  },
+  {
+    "revision": "8b29a360aa010bc7d089",
+    "url": "/cassaundra-io/static/css/main.9c50ddcd.chunk.css"
+  },
+  {
+    "revision": "8b8ad942310224a16029",
+    "url": "/cassaundra-io/static/css/2.01551523.chunk.css"
+  },
+  {
+    "revision": "7f1765849fb2e3451ba631b23303f179",
+    "url": "/cassaundra-io/index.html"
+  }
+];
