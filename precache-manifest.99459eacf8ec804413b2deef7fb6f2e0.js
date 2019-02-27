884f1b4a4ec97b274320773eba440f712b345dcf
Cassaundra <cass@cassaundra.io>
2019-02-27 09:51:07 -0800

    Updates

---
+++ precache-manifest.99459eacf8ec804413b2deef7fb6f2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "61d209c9510962c48048",
+    "url": "/static/js/main.61d209c9.chunk.js"
+  },
+  {
+    "revision": "8b8ad942310224a16029",
+    "url": "/static/js/2.8b8ad942.chunk.js"
+  },
+  {
+    "revision": "61d209c9510962c48048",
+    "url": "/static/css/main.9c50ddcd.chunk.css"
+  },
+  {
+    "revision": "8b8ad942310224a16029",
+    "url": "/static/css/2.01551523.chunk.css"
+  },
+  {
+    "revision": "aaacba3e1639d84e085b1240ebfc4f6a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cassaundra-io/precache-manifest.e8be23301d6ab913418125990cbcf63e.js"
+  "/precache-manifest.99459eacf8ec804413b2deef7fb6f2e0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cassaundra-io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
