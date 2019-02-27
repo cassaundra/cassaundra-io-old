cef4b27a2350638b6547ad3adde6c2364f1cb2ab
Cassaundra <cass@cassaundra.io>
2019-02-27 09:36:18 -0800

    Updates

--- precache-manifest.c2fec03b0750675fdcbdefa840fbe567.js
+++ precache-manifest.c2fec03b0750675fdcbdefa840fbe567.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/cassaundra-io/static/js/runtime~main.59945bae.js"
   },
   {
-    "revision": "f1239a0c6b20d856c584",
-    "url": "/cassaundra-io/static/js/main.f1239a0c.chunk.js"
+    "revision": "ff9206ae2d10655a52b2",
+    "url": "/cassaundra-io/static/js/main.ff9206ae.chunk.js"
   },
   {
-    "revision": "8b8ad942310224a16029",
-    "url": "/cassaundra-io/static/js/2.8b8ad942.chunk.js"
+    "revision": "b35f2f314bef9571946b",
+    "url": "/cassaundra-io/static/js/2.b35f2f31.chunk.js"
   },
   {
-    "revision": "f1239a0c6b20d856c584",
+    "revision": "ff9206ae2d10655a52b2",
     "url": "/cassaundra-io/static/css/main.9c50ddcd.chunk.css"
   },
   {
-    "revision": "8b8ad942310224a16029",
+    "revision": "b35f2f314bef9571946b",
     "url": "/cassaundra-io/static/css/2.01551523.chunk.css"
   },
   {
-    "revision": "27ef47e52a9cc599c7f496c0ac39cc0c",
+    "revision": "7f35fc3539c0e6027fcf84791c2f392c",
     "url": "/cassaundra-io/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cassaundra-io/precache-manifest.48a4f7ab415bf602040f73894edcdfc1.js"
+  "/cassaundra-io/precache-manifest.c2fec03b0750675fdcbdefa840fbe567.js"
 );
 
 workbox.clientsClaim();
