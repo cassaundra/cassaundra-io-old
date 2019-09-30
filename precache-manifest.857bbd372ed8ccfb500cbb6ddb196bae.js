ec8a96be5d8570258e27a7d13bc1e399af8fc936
cassbot <cassbot@fuckspam.email>
2019-09-30 22:28:19 +0000

    Updates

---
+++ precache-manifest.857bbd372ed8ccfb500cbb6ddb196bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21c0b6f1e6a819f9d118",
+    "url": "/static/js/main.5f89bc48.chunk.js"
+  },
+  {
+    "revision": "9a8ef8ce28051e56aee0",
+    "url": "/static/js/2.dc043e7d.chunk.js"
+  },
+  {
+    "revision": "21c0b6f1e6a819f9d118",
+    "url": "/static/css/main.a4255f0e.chunk.css"
+  },
+  {
+    "revision": "9a8ef8ce28051e56aee0",
+    "url": "/static/css/2.de5d6048.chunk.css"
+  },
+  {
+    "revision": "70dee5b6e79772c21f4b7073e4541282",
+    "url": "/index.html"
+  }
+];
