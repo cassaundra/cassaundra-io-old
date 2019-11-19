4c4ec3773fadbb83d80e1d9fc64345dac18d9142
cassbot <cassbot@fuckspam.email>
2019-11-19 13:17:54 +0000

    Updates

---
+++ precache-manifest.faf2ddadb5ae29cb472da4cefe909fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7852171b675077c1d7f9",
+    "url": "/static/js/main.97d2fe21.chunk.js"
+  },
+  {
+    "revision": "03b9fb6f1af5c5461a80",
+    "url": "/static/js/2.33f30f2d.chunk.js"
+  },
+  {
+    "revision": "7852171b675077c1d7f9",
+    "url": "/static/css/main.a4255f0e.chunk.css"
+  },
+  {
+    "revision": "03b9fb6f1af5c5461a80",
+    "url": "/static/css/2.dca2de1e.chunk.css"
+  },
+  {
+    "revision": "1818a4a27f336800ee601136f703f8b4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea068bce9f3ec795f7f2a0effa1afe9c.js"
+  "/precache-manifest.faf2ddadb5ae29cb472da4cefe909fc7.js"
 );
 
 workbox.clientsClaim();
