eeec9ad584318c82bf26f041daa3c48ad315722b
cassbot <cassbot@fuckspam.email>
2020-03-23 16:51:28 +0000

    Updates

--- precache-manifest.3a681908dc2a1ffb52cd16d0342fa646.js
+++ precache-manifest.3a681908dc2a1ffb52cd16d0342fa646.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a91735719bbff0a34b94fd63e3070452",
+    "revision": "f775bc6fd970d48ec5355064b2944225",
     "url": "/index.html"
   },
   {
-    "revision": "3c1179917d659f25ead1",
+    "revision": "89e30f83ac6c6532346f",
     "url": "/static/css/2.091d7497.chunk.css"
   },
   {
-    "revision": "3c29f8e43c6c8fb6907c",
+    "revision": "91c9e154c6a559fe3114",
     "url": "/static/css/main.0bc5aa6c.chunk.css"
   },
   {
-    "revision": "3c1179917d659f25ead1",
-    "url": "/static/js/2.8ad62591.chunk.js"
+    "revision": "89e30f83ac6c6532346f",
+    "url": "/static/js/2.7e3a8a0d.chunk.js"
   },
   {
     "revision": "bad553a4c937e71447fc90d8c8529c11",
-    "url": "/static/js/2.8ad62591.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7e3a8a0d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3c29f8e43c6c8fb6907c",
-    "url": "/static/js/main.7c2a4976.chunk.js"
+    "revision": "91c9e154c6a559fe3114",
+    "url": "/static/js/main.f833f8f8.chunk.js"
   },
   {
     "revision": "812ccd8ac57df0a46033",
