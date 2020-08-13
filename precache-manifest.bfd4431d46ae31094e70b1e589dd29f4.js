81bd417285cf16b8e6e7183faa5be5ade25198e7
cassbot <cassbot@fuckspam.email>
2020-08-13 00:12:15 +0000

    Updates

---
+++ precache-manifest.bfd4431d46ae31094e70b1e589dd29f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773b600b374259623a3ac869e463e2ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2be7014ce64b53c8dba",
+    "url": "/static/css/2.5a45da76.chunk.css"
+  },
+  {
+    "revision": "5e2ee7b4da57d7fe8021",
+    "url": "/static/css/main.0bc5aa6c.chunk.css"
+  },
+  {
+    "revision": "b2be7014ce64b53c8dba",
+    "url": "/static/js/2.1fb7323c.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.1fb7323c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2ee7b4da57d7fe8021",
+    "url": "/static/js/main.a0878979.chunk.js"
+  },
+  {
+    "revision": "812ccd8ac57df0a46033",
+    "url": "/static/js/runtime-main.76a36501.js"
+  }
+]);
